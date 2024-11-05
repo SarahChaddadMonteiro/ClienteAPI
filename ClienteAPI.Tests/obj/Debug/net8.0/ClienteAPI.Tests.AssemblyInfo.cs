a591a4903083750bd7f70337050649bd10fc3039
SarahChaddadMonteiro <sahchaddad@gmail.com>
2024-11-05 00:00:50 -0300

    corrigindo bugs

--- ClienteAPI.Tests/obj/Debug/net8.0/ClienteAPI.Tests.AssemblyInfo.cs
+++ ClienteAPI.Tests/obj/Debug/net8.0/ClienteAPI.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClienteAPI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5694c06f62e380cd9f49beb30dd74b0d710688c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClienteAPI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClienteAPI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Debug/net8.0/ClienteAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClienteAPI.AssemblyInfo.cs
@@ -7,16 +7,16 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-using System;
-using System.Reflection;
+//using System;
+//using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ClienteAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("ClienteAPI")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ClienteAPI")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+//[assembly: System.Reflection.AssemblyCompanyAttribute("ClienteAPI")]
+//[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]//
+//[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+//[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5694c06f62e380cd9f49beb30dd74b0d710688c")]
+//[assembly: System.Reflection.AssemblyProductAttribute("ClienteAPI")]
+//[assembly: System.Reflection.AssemblyTitleAttribute("ClienteAPI")]
+//[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
